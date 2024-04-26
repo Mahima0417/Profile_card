d04486fdafdfffb0a677b3365e9b221889ac8b69
MAHIMA <128350977+Mahima0417@users.noreply.github.com>
2024-04-26 07:13:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Profile from "./pcard";
+function App() {
+  return (
+    <div className="app"><Profile/></div>
+    
+  )
+}
+
+export default App;
---
+++ pcard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './pcard.css';
+
+const Profile =()=>{
+    return(
+        <div className='container'>
+            <div className='top'></div>
+            <div className='image'><img src="Snapchat-1356031511-01-removebg-preview.png" alt="no found"></img></div>
+            <div className='info'>My name is Mahima. I am currently pursuing MCA.  I have good knowledge of HTML, CSS nad javascript. Now I am learning react.js. </div>
+            
+        </div>
+    )
+}
+export default Profile;
